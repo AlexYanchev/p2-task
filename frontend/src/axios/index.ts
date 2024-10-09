8e22ba672e4aa314c129d44ff03ca3e810d107ab
Alexey Yanchev <yanchevalexey@yandex.ru>
2024-10-09 18:03:07 +0300

    Backend integrated

--- frontend/src/axios/index.ts
+++ frontend/src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://p2tasks-4wuq9v0q.b4a.run/api',
 });
 
 export default axiosInstance;
